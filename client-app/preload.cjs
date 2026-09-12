91d36aea26455fa92d0c51805d734eb34a020920
luis5afp <luis5afp@gmail.com>
2026-09-11 19:17:04 -0500

    Add Client preload bridge

---
+++ client-app/preload.cjs
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('userflex', {
+  bootstrap: () => ipcRenderer.invoke('userflex:bootstrap'),
+  login: (input) => ipcRenderer.invoke('userflex:login', input),
+  catalog: () => ipcRenderer.invoke('userflex:catalog'),
+  launchProfile: (profileId) => ipcRenderer.invoke('userflex:launch-profile', profileId),
+  logout: () => ipcRenderer.invoke('userflex:logout'),
+  onHeartbeat: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('userflex:heartbeat', listener);
+    return () => ipcRenderer.removeListener('userflex:heartbeat', listener);
+  },
+  onAuthInvalidated: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('userflex:auth-invalidated', listener);
+    return () => ipcRenderer.removeListener('userflex:auth-invalidated', listener);
+  },
+});
