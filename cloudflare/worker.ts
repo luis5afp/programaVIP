ebd769b18c2b81499d01cb5fe13b454fc2209abf
luis5afp <luis5afp@gmail.com>
2026-09-15 03:12:09 -0500

    Bump Worker API to 1.2.9

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -18,7 +18,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.2.8';
+const APP_VERSION = '1.2.9';
 
 async function api(request: Request, env: Env): Promise<Response> {
   const url = new URL(request.url);
