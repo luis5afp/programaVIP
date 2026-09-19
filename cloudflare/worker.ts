b2495424f3e01b4370523067abec22eb916c3add
luis5afp <luis5afp@gmail.com>
2026-09-19 02:06:06 -0500

    chore: bump server to 1.3.10

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -22,7 +22,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.3.9';
+const APP_VERSION = '1.3.10';
 
 async function api(request: Request, env: Env): Promise<Response> {
   const url = new URL(request.url);
