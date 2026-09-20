3f4c68ff67a8f892f7a0f21e685d31e5af71426a
luis5afp <luis5afp@gmail.com>
2026-09-19 19:16:55 -0500

    Bump admin API for 0.3.19 verification release

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -23,7 +23,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.4.4';
+const APP_VERSION = '1.4.5';
 
 function assertMinimumUserflowVersion(request: Request) {
   const version = clientVersionFrom(request);
