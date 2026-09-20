80c8e488802c64823a390a30ac676d0fea51728c
luis5afp <luis5afp@gmail.com>
2026-09-19 22:27:30 -0500

    Bump admin API for updater reliability release

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -23,7 +23,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.4.5';
+const APP_VERSION = '1.4.6';
 
 function assertMinimumUserflowVersion(request: Request) {
   const version = clientVersionFrom(request);
