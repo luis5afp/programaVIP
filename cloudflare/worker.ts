ae1ad1b977ebe7b0c25a44015e669500f5bbd02e
luis5afp <luis5afp@gmail.com>
2026-09-20 12:43:33 -0500

    Bump admin API for automatic updater install

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -23,7 +23,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.4.7';
+const APP_VERSION = '1.4.8';
 
 function assertMinimumUserflowVersion(request: Request) {
   const version = clientVersionFrom(request);
