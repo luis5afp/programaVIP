a9622309465a0a13fc9e1000242b7881225c3a94
luis5afp <luis5afp@gmail.com>
2026-09-21 08:24:07 -0500

    Bump Admin API for Session Keeper

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -23,7 +23,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.4.12';
+const APP_VERSION = '1.4.13';
 
 function assertMinimumUserflowVersion(request: Request) {
   const version = clientVersionFrom(request);
