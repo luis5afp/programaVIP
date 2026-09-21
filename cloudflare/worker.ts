b6a348189b0a3d4a62c94f6cbdada0a1ee33ef1b
luis5afp <luis5afp@gmail.com>
2026-09-21 14:38:22 -0500

    Bump Admin API for Keeper event triggers

--- cloudflare/worker.ts
+++ cloudflare/worker.ts
@@ -23,7 +23,7 @@ import {
   withSecurity,
 } from './lib/core';
 
-const APP_VERSION = '1.4.14';
+const APP_VERSION = '1.4.15';
 
 function assertMinimumUserflowVersion(request: Request) {
   const version = clientVersionFrom(request);
